3903026f984cf5545227dbe6b1cdfd048dc906e0
Sergio Almeida <sergioalmeida29.05@gmail.com>
2025-05-19 09:54:16 +0100

    Bug Fixes in image

--- src/Image.cpp
+++ src/Image.cpp
@@ -1,30 +1,26 @@
 #include "Image.hpp"
 
 namespace prog {
-    Image::Image(int w, int h, const Color &fill) {
-
+    Image::Image(int w, int h, const Color &fill) : w(w), h(h){
+      pixels.resize(w * h,  fill);
     }
 
     Image::~Image() {
     }
 
     int Image::width() const {
-        return -1;
+        return w;
     }
 
     int Image::height() const {
-        return -1;
+        return h;
     }
 
-  // TODO: remove this DUMMY_color variable once you have appropriate fields for representing image pixels.
-  Color DUMMY_color;
-
-
     Color &Image::at(int x, int y) {
-		return DUMMY_color;
+        return pixels[y * w + x];
     }
 
     const Color &Image::at(int x, int y) const {
-        return DUMMY_color;
+        return pixels[y * w + x];
     }
 }
